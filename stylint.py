ace828a6cd3ba350d79b43e9db8a256e371f192b
Ian Korf <ifkorf@ucdavis.edu>
2024-01-26 16:45:05 -0800

    up

--- stylint.py
+++ stylint.py
@@ -4,11 +4,11 @@ import sys
 if len(sys.argv) != 2: sys.exit(f'usage: {sys.argv[0]} file.py')
 
 issues = {
-	'tabs':    '^( +\w+)',
-	'space (': '(\w+\s+\()',
-	'comma':   '(\w+,\w+)',
-	'mixed':   '([a-z][A-Z])',
-	'long':    '(^[^#].{81,}$)',
+	'tabs':  '^( +\w+)',
+	'space': '(\w+\s+\()',
+	'comma': '(\w+,\w+)',
+	'mixed': '([a-z][A-Z])',
+	'long':  '(^[^#].{81,}$)',
 }
 
 with open(sys.argv[1]) as fp:
