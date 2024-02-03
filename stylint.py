8b46cea656166a6860557ae528b5567e36a1b467
Ian Korf <ifkorf@ucdavis.edu>
2024-02-03 11:21:42 -0800

    up

--- stylint.py
+++ stylint.py
@@ -1,14 +1,30 @@
+#!/usr/bin/env python3
+
+import argparse
 import re
 import sys
 
-if len(sys.argv) != 2: sys.exit(f'usage: {sys.argv[0]} file.py')
+parser = argparse.ArgumentParser(description='MCB185 style-checker')
+parser.add_argument('file', type=str, help='path to python file')
+parser.add_argument('-r', '--report-level', type=int, default=2,
+	metavar='[0-2]', help='reporting level [%(default)i]')
+arg = parser.parse_args()
 
 issues = {
 	'indent': r'^( +\w+)',
-	'space':  r'(\w+)\s+\(',
+	'space':  r'(\w+\s+\()',
+	'white':  r'(\t | \t)',
 	'comma':  r'(\w+,\w+)',
 	'mixed':  r'([a-z]+[A-Z]+|[A-Z]+[a-z]+)',
-	'length': r'(^[^#].{81,}$)',
+}
+
+explain = {
+	'indent': 'not allowed to indent with spaces (check editor settings)',
+	'space': 'there should be no space between a function name and opening (',
+	'white': 'mixing spaces and tabs in whitespace can look weird',
+	'comma': 'there should be a space after a comma',
+	'mixed': 'use snake_case instead of mixedCase',
+	'max80': 'line length exceeds 80 characters',
 }
 
 keywords = (
@@ -20,27 +36,72 @@ keywords = (
 	'return', 'yield',
 )
 
-clean = True
-with open(sys.argv[1]) as fp:
-	n = 0
-	for line in fp:
-		n += 1
-		if line.startswith('#'): continue
-		if line.startswith('"""') or line.startswith("'''"):
-			for line in fp:
-				n += 1
-				if line.startswith('"""') or line.startswith("'''"): break
-		
-		for problem, pattern in issues.items():
-			m = re.search(pattern, line)
-			if m:
-				found = m.group(1)
-				if problem == 'space' and found in keywords: continue
-				if problem == 'mixed' and found in keywords: continue
-				if problem == 'mixed' and '#' in line: continue
-				if problem == 'mixed' and '"' in line: continue
-				if problem == 'mixed' and "'" in line: continue
-				print(f'{n}\t{problem}\t"{line[:-1]}"')
-				clean = False
+def in_string(found, line):
+	p1 = line.find("'")
+	p2 = line.find(found, p1)
+	p3 = line.find("'", p2)
+	if p1 < p2 and p2 < p3: return True
+	return False
 
-if clean: print(sys.argv[1], 'is clean')
+problems = []
+fp = open(sys.argv[1])
+n = 0
+for line in fp:
+	n += 1
+		
+	# comments: only check for length
+	if line.startswith('#'):
+		if len(line) > 80: problems.append( (n, 'max80', line) )
+		else:              continue
+
+	# long comments: also only length
+	if line.startswith('"""') or line.startswith("'''"):
+		for line in fp:
+			n += 1
+			if len(line) > 80: problems.append( (n, 'max80', line) )
+			if line.startswith('"""') or line.startswith("'''"): break
+		continue
+	
+	# style issues
+	if len(line) > 80: problems.append( (n, 'max80', line) )
+	for problem, pattern in issues.items():
+		m = re.search(pattern, line)
+		if m:
+			found = m.group(1)
+			if problem == 'space':
+				kwfound = False
+				for kw in keywords:
+					if found.startswith(kw):
+						kwfound = True
+						break
+				if kwfound: continue
+			if problem == 'space' and in_string(found, line): continue
+			if problem == 'mixed' and found in keywords: continue
+			if problem == 'mixed' and '#' in line: continue
+			if problem == 'mixed' and '"' in line: continue
+			if problem == 'mixed' and "'" in line: continue
+			line = line.rstrip()
+			problems.append( (n, problem, line) )
+
+# report
+
+if arg.report_level >= 0:
+	if len(problems) == 0:
+		print(sys.argv[1], 'is clean')
+		sys.exit(0)
+	else:
+		print(sys.argv[1], 'has style issues')
+
+if arg.report_level >= 1:
+	print('Line Report')
+	tell = {}
+	for n, prob, line in problems:
+		print(f'  Line {n} ({prob}): {line[:50]}')
+		tell[prob] = True
+
+if arg.report_level >= 2:
+	print('Detailed Explanations')
+	for prob in tell:
+		print(f'  {prob}: {explain[prob]}')
+
+
